1eaef6233c3b10d185834a4284a813244ba093ec
PerfectELK <Losikoban@Gmail.com>
2019-06-17 09:34:07 +0300

    is ready

--- src/site/default/blocks/footer/common_scripts.js
+++ src/site/default/blocks/footer/common_scripts.js
@@ -3,7 +3,7 @@ var random_place__word_generate = function(){
 
     let wordArr = [
         'tar -xvf \'path\'','tar -cvzf \'arch_name\' \'path\'',
-        'Владосиус, заходи, заебал',
+        'Vladosius go here, zaebal',
         'ssh -p \'username\'@\'ip or domain\'',
         'mysql -u \'user\' -p \'db_name\' < \'sql_dump\'',
         'mysqldump -u \'user\' -p  \'db_name\' > \'path\''
@@ -11,8 +11,8 @@ var random_place__word_generate = function(){
 
     var phrase = wordArr[Math.floor(Math.random() * wordArr.length)];
 
-    var width = Math.floor(Math.random() * window.innerWidth);
-    var height = Math.floor(Math.random() * window.innerHeight);
+    var width = Math.floor(Math.random() * window.innerWidth) - 20;
+    var height = Math.floor(Math.random() * window.innerHeight) - 10;
     var rotate = Math.floor(Math.random() * 30) - 15;
     var elem = $(document.createElement('div')).addClass('rand_phrase');
     elem.css({'top':`${height}px`,'left':`${width}px`, 'transform' : `rotate(${rotate}deg)`});
